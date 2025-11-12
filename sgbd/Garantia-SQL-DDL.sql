8dc2d744c696395b1c60f6d6cfae6cde87e6619a
Thiago Domingos Venturim Ribeiro dos Santos <thiagodomingosventutim@gmail.com>
2025-11-12 18:05:32 -0300

    Tabelas Cliente e Produto

--- sgbd/Garantia-SQL-DDL.sql
+++ sgbd/Garantia-SQL-DDL.sql
@@ -6,9 +6,9 @@ CREATE TABLE Cliente (
     tipo_cliente ENUM('F', 'J') NOT NULL, 
     nome_solicitante VARCHAR(100) NOT NULL,
     email_solicitante VARCHAR(100) NOT NULL,
-    rua VARCHAR(100) NOT NULL,
-    endereco VARCHAR(100) NOT NULL,
-    bairro VARCHAR(50) NOT NULL,
+    rua VARCHAR(100)  ,
+    endereco VARCHAR(100)  ,
+    bairro VARCHAR(50)  ,
     cpf int UNIQUE,
     cnpj int UNIQUE,
     nome_fantasia VARCHAR(100),
@@ -17,4 +17,20 @@ CREATE TABLE Cliente (
         (tipo_cliente = 'F' AND cpf IS NOT NULL AND cnpj IS NULL) OR
         (tipo_cliente = 'J' AND cnpj IS NOT NULL AND cpf IS NULL)
     )
+);
+
+CREATE TABLE Produto(
+    numero_da_nota_fiscal int PRIMARY KEY NOT NULL;
+    id_produto INT PRIMARY KEY AUTO_INCREMENT,
+    nome_do_produto VARCHAR(100) NOT NULL,
+    quantidade INT NOT NULL,
+    data_da_entrega DATE,
+    foto_do_produto VARBINARY(MAX);
+);
+
+CREATE TABLE Setor(
+    id_setor int PRIMARY KEY NOT NULL,
+    nome_setor VARCHAR(100) UNIQUE NOT NULL,
+    responsavel  VARCHAR(100)  NOT NULL,
+    email_do_responsavel VARCHAR(100)  NOT NULL
 );
